066fa971503e2b460fedf8b5ee924fa836cadaaf
suryanovara <suryanovara@gmail.com>
2026-07-02 12:11:23 +0800

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -14,4 +14,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-console.log("Firebase berhasil terhubung!");
+console.log("Firebase berhasil terhubung!", db);
